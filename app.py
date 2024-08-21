0c6262009bf978a0f78423b9801f71d3edd06a7a
Teoh TeikToe <teohteiktoe@gmail.com>
2024-08-21 16:46:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
 from flask import Flask,render_template,request
+import google.generativeai as palm
+import os
+import openai
+
+api = ""
+palm.configure(api_key=api)
+model = {"model": "models/chat-bison-001"}
+
+os.environ["OPENAI_API_KEY"] = ""
+client = openai.OpenAI()
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
+    return(render_template("index.html"))
+
+@app.route("/ai_agent", methods=["GET","POST"])
+def ai_agent():
+    return(render_template("ai_agent.html"))
+
+@app.route("/ai_agent_reply", methods=["GET","POST"])
+def ai_agent_reply():
+    q = request.form.get("q")
+    r = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": q}],
+    )
+    r = r.choices[0].message.content
+    return(render_template("ai_agent_reply.html",r=r))
+
+@app.route("/prediction", methods=["GET","POST"])
+def prediction():
     return(render_template("index.html"))
 
 if __name__ == "__main__":
